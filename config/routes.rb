e3ffc632e11c1bf23387bad8d64296ddfebdf2ef
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-18 20:48:55 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories, only: :index do
-    resources :stages, only: :index
+  scope :api, defaults: { format: :json } do
+    resources :categories, only: :index do
+      resources :stages, only: :index
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
