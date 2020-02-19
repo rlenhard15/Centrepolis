2244f9a9cd1369768a852a47e99ba663d9d77045
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-19 15:26:16 +0200

    delete resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :categories, only: :index
+  get '/categories', to: 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
