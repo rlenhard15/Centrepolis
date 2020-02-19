f524c57e65e6e68c41bb3f2970b67536c48e1e7b
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-19 16:26:22 +0200

    fix conflict

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,10 @@
 class CategoriesController < ApplicationController
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> master
   # GET /categories
   def index
+    
     @categories = Category.all
 
     render json: @categories
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 end
