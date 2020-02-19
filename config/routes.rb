2c454b42c9414242af3f1c8ded8c58c67d232571
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-19 18:25:01 +0200

    add exceptions for defaults routesin tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     resources :categories, only: :index do
       resources :stages, only: :index do
-        resources :tasks do
+        resources :tasks, except: [:index, :show, :new, :edit, :create, :update, :destroy] do
           member do
-            get 'mark_as_completed'
-            get 'start_task'
+            get :mark_as_completed
+            get :start_task
           end
         end
       end
