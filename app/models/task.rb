bb89c64f12bb8de63703a74d2f883a9d8bf8ecf8
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-19 18:55:58 +0200

    create many to many ralationship and set up destroy

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :stage
+  has_many :user_tasks, dependent: :destroy
+  has_many :users, through: :user_tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_124302) do
+ActiveRecord::Schema.define(version: 2020_02_19_164309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_02_19_124302) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.integer "stage_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
