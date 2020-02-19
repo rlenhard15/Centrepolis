e0f0bdedd9f42ae1096e7aaa51906ef31f21fa4c
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-19 18:59:34 +0200

    change array routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     resources :categories, only: :index do
       resources :stages, only: :index do
-        resources :tasks, except: [:index, :show, :new, :edit, :create, :update, :destroy] do
+        resources :tasks, except: %i[index show new edit create update destroy] do
           member do
             get :mark_as_completed
             get :start_task
