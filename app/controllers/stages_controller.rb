c2950555cba6fb2ae9bc320cffd3bdfdf8948170
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-24 16:42:10 +0200

    change index action for stages

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,15 +1,16 @@
 class StagesController < ApplicationController
-  before_action :set_category, only: :index
+  before_action :set_sub_category, only: :index
 
   # GET /stages
   def index
-    @stages = @category.list_of_stages
+    @stages = @sub_category.stages
 
     render json: @stages.to_json(methods: :tasks_for_category)
   end
 
   private
-  def set_category
+  def set_sub_category
     @category = Category.find(params[:category_id])
+    @sub_category = @category.sub_categories.find(params[:sub_category_id])
   end
 end
