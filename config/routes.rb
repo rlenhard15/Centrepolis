6429310fd49b681becb2fad931937c0873471653
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-24 16:15:26 +0200

    update tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   scope :api, defaults: { format: :json } do
     resources :categories, only: :index do
-      resources :stages, only: :index do
-        resources :tasks do
-          member do
-            get :mark_as_completed
-            get :start_task
+      resources :sub_categories, only: :index do
+        resources :stages, only: :index do
+          resources :tasks do
+            member do
+              get :mark_as_completed
+              get :start_task
+            end
           end
         end
       end
