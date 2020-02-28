c9fdd43c0bd48136a8d16ebd6b6979742516bddc
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-02-28 16:50:43 +0200

    fix code

--- config/initializers/core_extensions/devise/strategies/json_web_token.rb
+++ config/initializers/core_extensions/devise/strategies/json_web_token.rb
@@ -7,7 +7,7 @@ module Devise
 
       def authenticate!
         failed = check_fail
-        (failed || user_from_db.nil?) ? failed : (success! @user_from_db)
+        (failed || user_from_db.nil?) ? failed : (success! user_from_db)
       end
 
       protected
