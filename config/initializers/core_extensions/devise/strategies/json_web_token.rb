2af53c4973d19d8589d6261fa138bbcde5635a94
shatskiym <michael.shatskiy@gmail.com>
2020-03-04 20:19:37 +0200

    Fixed typo

--- config/initializers/core_extensions/devise/strategies/json_web_token.rb
+++ config/initializers/core_extensions/devise/strategies/json_web_token.rb
@@ -18,7 +18,7 @@ module Devise
 
       def token_from_header
         strategy, token = request.headers['Authorization'].split(' ')
-        return nil if (strategy || '').downcase != 'bearer'
+        return nil if (strategy || '').downcase != 'Bearer'
         token
       end
 
