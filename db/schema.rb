8e4cad01329db7d7fa41922ea53f32ce1f02ce27
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-03-15 16:44:33 +0200

    delet polymorphic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_195114) do
+ActiveRecord::Schema.define(version: 2020_03_14_214753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,26 +21,11 @@ ActiveRecord::Schema.define(version: 2020_03_14_195114) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.integer "degree_access"
-    t.string "department"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "accessment_id"
-  end
-
-  create_table "original_users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "team"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "stages", force: :cascade do |t|
@@ -74,10 +59,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_195114) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "meta_id"
-    t.string "meta_type"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["meta_id", "meta_type"], name: "index_users_on_meta_id_and_meta_type"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
