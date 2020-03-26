86e806b4c0fabee08093e4cf7eca3ee6fc806c3f
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-03-26 20:15:28 +0200

    fix code

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,5 +1,5 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
   has_many :stages, dependent: :destroy
-  has_many :sub_category_progresses, dependent: :destroy
+  has_many :sub_category_progress, dependent: :destroy
 end
