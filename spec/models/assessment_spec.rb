b68e01b51ef50b1f3d8b869c2a90debf9f382cae
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-04-08 15:18:14 +0300

    create test

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -7,6 +7,32 @@ RSpec.describe Assessment, type: :model do
     it { should have_many(:assessment_progresses).dependent(:destroy) }
   end
 
+  describe "Method 'with_assessment_progresses'" do
+    let!(:admin)                     { create(:admin) }
+      let!(:customer)                { create(:customer, created_by: admin.id) }
+    let!(:assessment)               { create(:assessment) }
+      let!(:assessment_progress_1)   { create(:assessment_progress, customer_id: customer.id, assessment_id: assessment.id, risk_value: 3.812) }
+
+    it "return assessments with risk_value for customer" do
+      # assessment_with_risk = Assessment.with_assessment_progresses(customer.id)
+       byebug
+
+      expect(assessment_with_risk.length).to eq(1)
+      expect(Assessment.with_assessment_progresses(customer.id)).to eq(
+        [
+          {
+            "id"=> assessment.id,
+            "name"=> assessment.name,
+            "created_at"=> assessment.created_at,
+            "updated_at"=> assessment.updated_at,
+            "risk_value"=> assessment_progress_1.risk_value
+          }
+        ]
+      )
+
+    end
+  end
+
   describe "Method 'description_with_child_models'" do
     let!(:assessment)   { create(:assessment) }
     let!(:category)     { create(:category, assessment_id: assessment.id) }
