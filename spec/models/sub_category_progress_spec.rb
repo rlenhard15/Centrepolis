9c76bca0e6bb37ea32c0187887223714c2501ac3
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-04-13 17:45:22 +0300

    create tests for sub_category_progresses

--- spec/models/sub_category_progress_spec.rb
+++ spec/models/sub_category_progress_spec.rb
@@ -5,4 +5,37 @@ RSpec.describe SubCategoryProgress, type: :model do
     it { should belong_to(:customer).without_validating_presence }
     it { should belong_to(:sub_category).without_validating_presence }
   end
+
+  describe "Method 'correct_values_for_current_stage_id'" do
+    let!(:admin)            { create(:admin) }
+      let!(:customer)       { create(:customer, created_by: admin.id) }
+    let!(:assessment)       { create(:assessment) }
+      let!(:category)       { create(:category, assessment_id: assessment.id) }
+        let!(:sub_category) { create(:sub_category, category_id: category.id) }
+          let!(:stage)      { create(:stage, sub_category_id: sub_category.id) }
+
+    it "return 1 if current_stage_id exists in stages_ids" do
+      params = {
+        customer_id: customer.id,
+        sub_category_id: sub_category.id,
+        current_stage_id: stage.id
+      }
+
+      SubCategoryProgress.create(params)
+
+      expect(SubCategoryProgress.count).to eq(1)
+    end
+
+    it "return 0 if current_stage_id doesn't exist in stages_ids" do
+      params = {
+        customer_id: customer.id,
+        sub_category_id: 1,
+        current_stage_id: stage.id
+      }
+
+      SubCategoryProgress.create(params)
+
+      expect(SubCategoryProgress.count).to eq(0)
+    end
+  end
 end
--- spec/models/sub_category_spec.rb
+++ spec/models/sub_category_spec.rb
@@ -6,4 +6,33 @@ RSpec.describe SubCategory, type: :model do
     it { should have_many(:stages).dependent(:destroy) }
     it { should have_many(:sub_category_progresses).dependent(:destroy) }
   end
+
+  describe "Method 'with_stages_progresses'" do
+    let!(:admin)            { create(:admin) }
+      let!(:customer)       { create(:customer, created_by: admin.id) }
+    let!(:assessment)       { create(:assessment) }
+      let!(:category)       { create(:category, assessment_id: assessment.id) }
+        let!(:sub_category) { create(:sub_category, category_id: category.id) }
+          let!(:stage)      { create(:stage, sub_category_id: sub_category.id) }
+            let!(:sub_category_progress) { create(:sub_category_progress, sub_category_id: sub_category.id, current_stage_id: stage.id, customer_id: customer.id) }
+
+    it "return stages progress for sub_category" do
+      sub_category_with_progress = SubCategory.with_stages_progresses(customer.id).select("sub_categories.*, sub_category_progresses.*").as_json
+
+      recursively_delete_timestamps(sub_category_with_progress)
+
+      expect(sub_category_with_progress).to eq(
+        [
+          {
+            "id"=> sub_category_progress.id,
+            "title"=> sub_category.title,
+            "category_id"=> sub_category.category_id,
+            "customer_id"=> sub_category_progress.customer_id,
+            "sub_category_id"=> sub_category_progress.sub_category_id,
+            "current_stage_id"=> sub_category_progress.current_stage_id
+          }
+        ]
+      )
+    end
+  end
 end
