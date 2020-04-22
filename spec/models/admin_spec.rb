4262d46231ae018247b484766e50a725e685e285
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-04-22 17:21:05 +0300

    fix code

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Admin, type: :model do
   describe "Associations" do
     it { should have_many(:customers).with_foreign_key('created_by') }
+    it { should have_many(:tasks).with_foreign_key('created_by') }
   end
 end
