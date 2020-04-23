2032225abb716b9313fb7653bc423fa9bb857cd5
shatskiym <michael.shatskiy@gmail.com>
2020-04-23 15:30:13 +0300

    Cleared code

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :stage
   belongs_to :admin, foreign_key: "created_by"
   belongs_to :customer, foreign_key: "user_id"
-  
+
   enum status: [:started, :completed]
   enum priority: [:low, :medium, :high]
 
