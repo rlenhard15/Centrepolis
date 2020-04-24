76b5a0d659b7104d093d38f00dbdd89bd728e4ea
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-04-24 13:23:25 +0300

    create Notification model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_091314) do
+ActiveRecord::Schema.define(version: 2020_04_24_101640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_091314) do
   create_table "notifications", force: :cascade do |t|
     t.integer "task_id"
     t.integer "customer_id"
-    t.boolean "read"
+    t.boolean "read", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
