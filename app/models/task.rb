06b6572ca499316e82867225b375f254dc80002e
shatskiym <michael.shatskiy@gmail.com>
2020-04-27 18:31:27 +0300

    Fixed typo

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,7 +9,7 @@ class Task < ApplicationRecord
 
   scope :with_all_required_info_for_tasks, -> {
     joins(stage: [sub_category: [category: :assessment]]).select(
-      "tasks.id, tasks.title, tasks.priority, tasks.due_date, task.status,
+      "tasks.id, tasks.title, tasks.priority, tasks.due_date, tasks.status,
       assessments.name AS master_assessment,
       categories.title AS category,
       sub_categories.title AS sub_category,
