9fa4328f4cb03518a9215e042d486a882773bb37
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-04-28 12:57:38 +0300

    fix code

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -29,7 +29,7 @@ class NotificationsController < ApplicationController
     render json: policy_scope(Notification).with_task_and_admin_info
   end
 
-  api :PUT, 'api/:id/mark_as_readed', "Update read status to readed"
+  api :PUT, 'api/:id/mark_as_readed', "Update read status og notification to readed"
   param :id, Integer, desc: "id of notification",  required: true
 
   description <<-DESC
