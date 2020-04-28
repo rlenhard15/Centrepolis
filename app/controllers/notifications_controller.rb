3d17f0490737090418fef45e0814f4f549faef70
shatskiym <michael.shatskiy@gmail.com>
2020-04-28 18:36:36 +0300

    Fixed route for mark notification as read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -29,7 +29,7 @@ class NotificationsController < ApplicationController
     render json: policy_scope(Notification).with_task_and_admin_info
   end
 
-  api :PUT, 'api/:id/mark_as_readed', "Update read status of notification to true"
+  api :PUT, 'api/notifications/:id/mark_as_readed', "Update read status of notification to true"
   param :id, Integer, desc: "id of notification",  required: true
 
   description <<-DESC
