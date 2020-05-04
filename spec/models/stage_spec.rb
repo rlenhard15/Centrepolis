f50967e03bd93528a634d3771f267dfad13eb7f3
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-05-04 13:28:44 +0300

    add tests for stages

--- spec/models/stage_spec.rb
+++ spec/models/stage_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Stage, type: :model do
     it { should belong_to(:sub_category) }
     it { should have_many(:tasks).dependent(:destroy) }
   end
+
+  describe "validates" do
+    it { should validate_uniqueness_of(:position).scoped_to(:sub_category_id).with_message("should be uniq for one sub_category") }
+  end
 end
