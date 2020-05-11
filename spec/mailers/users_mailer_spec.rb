76e0af5b536522b53ab3f4dddb3dab5ecbc0470a
shatskiym <michael.shatskiy@gmail.com>
2020-05-11 17:08:28 +0300

    Added stubs for env vars

--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe UsersMailer, type: :mailer do
   let!(:admin)    {create(:admin)}
   let!(:customer) {create(:customer, created_by: admin.id)}
   let!(:params)   {ActionController::Parameters.new({customer: customer})}
-  before {params.permit!}
 
   let!(:mail) {UsersMailer.with(params).email_for_restore_password}
+
+  before do
+    params.permit!
+    allow(ENV).to receive(:[]).with('SENDER_EMAIL').and_return('sender@mail.com')
+    allow(ENV).to receive(:[]).with('ADMIN_PANEL_HOST').and_return('http://localhost:3000')
+  end
 
   it 'renders the subject' do
     expect(mail.subject).to eq("You have been invited to Trello forms")
@@ -17,6 +22,6 @@ RSpec.describe UsersMailer, type: :mailer do
   end
 
   it 'renders the sender email' do
-    expect(mail.from).to eq([ENV['SENDER_EMAIL']])
+    expect(mail.from).to eq(['sender@mail.com'])
   end
 end
