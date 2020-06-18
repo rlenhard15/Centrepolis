9ed77abe80e0db0b05da4cc8cdb43668e34f6f9c
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-06-18 17:41:19 +0300

    fixcode

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,13 +1,5 @@
 class CustomFailure < Devise::FailureApp
-  def redirect_url
-    error_401_url
-  end
-
   def respond
-    if http_auth?
-      http_auth
-    else
-      redirect
-    end
+    http_auth
   end
 end
