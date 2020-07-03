3a62a70519494d8a47c872aeed01c5b46a2ce25d
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-07-03 17:30:44 +0300

    create tests for check authenticate with accelerators header

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -10,12 +10,17 @@ RSpec.describe ApplicationController, type: :controller do
     Rails.application.reload_routes!
   end
 
-  let!(:user) { create(:user) }
+  let!(:accelerator) { create(:accelerator) }
+  let!(:user)        { create(:user, accelerator_id: accelerator.id) }
 
   shared_examples "a dynamic method" do |method_name = "check_auth"|
+
     let(:create_method_application_controller) do
+      ApplicationController.define_method(:accelerator_id) do
+        accelerator_id = request.headers['Accelerator-Id'].to_i
+      end
       ApplicationController.define_method(method_name) do
-        response_auth =  (current_user.present?)? "success login" : "faild login"
+        response_auth =  ((current_user.present?)  && current_user.accelerator_id == accelerator_id) ?  "success login" : "faild login"
         render json: response_auth.to_json
       end
     end
@@ -23,11 +28,21 @@ RSpec.describe ApplicationController, type: :controller do
 
   describe "check authenticate of user" do
     include_context "a dynamic method"
-    it 'return success message' do
+    it 'return success message if accelerator_id belongs to user' do
       create_method_application_controller
+      allow_any_instance_of(ApplicationController).to receive(:accelerator_id).and_return(accelerator.id)
       sign_in user
       get :check_auth
       expect(JSON.parse(response.body)).to eq("success login")
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'return faild login message if accelerator_id doesnnt belong to user' do
+      create_method_application_controller
+      allow_any_instance_of(ApplicationController).to receive(:accelerator_id).and_return(rand(1..600))
+      sign_in user
+      get :check_auth
+      expect(JSON.parse(response.body)).to eq("faild login")
       expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Associations" do
     it { should have_many(:tasks).dependent(:destroy) }
+    it { should belong_to(:user) }
   end
 
   describe "Method 'payload'" do
