8230e6c00e1ad18b640703ac05cfcae87dffb063
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-07-03 18:52:57 +0300

    fix code

--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe UsersMailer, type: :mailer do
-  let!(:accelerator) {create(:accelerator)}
+  let!(:accelerator) {create(:accelerator, hostname: 'FUZEHUB_HOST')}
   let!(:admin)       {create(:admin, accelerator_id: accelerator.id)}
   let!(:customer)    {create(:customer, created_by: admin.id, accelerator_id: accelerator.id)}
   let!(:params)      {ActionController::Parameters.new({customer: customer})}
@@ -10,12 +10,14 @@ RSpec.describe UsersMailer, type: :mailer do
 
   before do
     params.permit!
+    allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('SENDER_EMAIL').and_return('sender@mail.com')
     allow(ENV).to receive(:[]).with('ADMIN_PANEL_HOST').and_return('http://localhost:3000')
+    allow(ENV).to receive(:[]).with('FUZEHUB_HOST').and_return('fuzehub_host')
   end
 
   it 'renders the subject' do
-    expect(mail.subject).to eq("You have been invited to Trello forms")
+    expect(mail.subject).to eq("You have been invited to RAMP Client Business Planning Support")
   end
 
   it 'renders the receiver email' do
