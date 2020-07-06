562a6cd2dcda30629c4ee9d052677f94fc02a321
ants_11 <anastasia.syrovatskaa@gmail.com>
2020-07-06 16:33:11 +0300

    fix code

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
   before do
-    allow_any_instance_of(ApplicationController).to receive(:authenticate_user!).and_return("skip_authenticate")
     Rails.application.routes.draw do
-      post 'api/application', to: 'application#check_auth', as: "check_auth"
+      get 'api/application', to: 'application#check_auth', as: "check_auth"
     end
   end
   after do
@@ -17,16 +16,8 @@ RSpec.describe ApplicationController, type: :controller do
   shared_examples "a dynamic method" do |method_name = "check_auth"|
 
     let(:create_method_application_controller) do
-      ApplicationController.define_method(:accelerator_id) do
-        accelerator_id = request.headers['Accelerator-Id'].to_i
-      end
       ApplicationController.define_method(method_name) do
-        user = User.find_by_email(params[:user][:email])
-        if user.accelerator_id == accelerator_id
-          render json: "success login"
-        else
-          render json: "faild login"
-        end
+        render json: "succed!"
       end
     end
   end
@@ -36,23 +27,23 @@ RSpec.describe ApplicationController, type: :controller do
     let!(:auth_token) { JwtWrapper.encode(user_id: user.id) }
 
     it 'return success message if accelerator_id belongs to user' do
-      params = ActionController::Parameters.new({ user: { email: user.email, password: user.password }})
       request.headers.merge!({ "Accelerator-Id": "#{accelerator.id}", "Authentication": "Bearer #{auth_token}" })
-      params.permit!
+      sign_in user
       create_method_application_controller
-      post :check_auth, params: params
-      expect(response.body).to eq("success login")
+      get :check_auth
+      expect(response.body).to eq("succed!")
+      expect(response.status).to eq(200)
       expect(response.content_type).to eq('application/json; charset=utf-8')
     end
 
     it 'return faild login message if accelerator_id doesnt belong to user' do
-      params = ActionController::Parameters.new({ user: { email: user.email, password: user.password }})
-      request.headers.merge!({ "Accelerator-Id": rand(1..600), "Authentication": "Bearer #{auth_token}" })
-      params.permit!
+      request.headers.merge!({ "Accelerator-Id": 1, "Authentication": "Bearer #{auth_token}" })
+      sign_in user
       create_method_application_controller
-      post :check_auth, params: params
-      expect(response.body).to eq("faild login")
-      expect(response.content_type).to eq('application/json; charset=utf-8')
+      get :check_auth
+      expect(response.body).to eq("You need to sign in or sign up before continuing.")
+      expect(response.status).to eq(401)
+      expect(response.content_type).to eq('text/html; charset=utf-8')
     end
   end
 end
