d55fe2ace66e937854fcea915c8232852a64a1d6
nastya218 <anastasia.syrovatskaa@gmail.com>
2021-05-14 22:34:07 +0300

    fix code

--- db/migrate/20210514171103_replace_data_for_startups.rb
+++ db/migrate/20210514171103_replace_data_for_startups.rb
@@ -4,10 +4,8 @@ class ReplaceDataForStartups < ActiveRecord::Migration[6.0]
 
     members.each do |m|
       AssessmentProgress.where(member_id: m.id)&.update_all(member_id: m.startup_id)
-      # assessment_progresses.each { |ap| ap.update(member_id: m.startup_id) }
 
       SubCategoryProgress.where(member_id: m.id)&.update_all(member_id: m.startup_id)
-      # sub_category_progresses.each { |scp| scp.update(member_id: m.startup_id) }
     end
 
     puts "Data was replaced"
