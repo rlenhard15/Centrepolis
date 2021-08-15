6ec5c15c2ff591ae709b3833d1a434c853f1eac0
nastya218 <anastasia.syrovatskaa@gmail.com>
2021-08-15 22:57:00 +0300

    fix migrations

--- db/migrate/20210412082926_create_table_users_tasks.rb
+++ db/migrate/20210412082926_create_table_users_tasks.rb
@@ -3,6 +3,7 @@ class CreateTableUsersTasks < ActiveRecord::Migration[6.0]
     create_table :task_users do |t|
       t.integer :task_id
       t.integer :user_id
+      t.boolean :creator, default: false
 
       t.timestamps
     end
--- db/migrate/20210621110019_add_creator_attribute_to_the_model.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatorAttributeToTheModel < ActiveRecord::Migration[6.0]
-  def change
-    add_column :task_users, :creator, :boolean, default: false
-  end
-end
